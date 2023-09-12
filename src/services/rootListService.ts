b9aa0e17790bff13ff96b914488dc4d2e7dd808c
repmacrypn <artik3267@gmail.com>
2023-09-12 21:11:41 +0300

    feat: added list service

---
+++ src/services/rootListService.ts
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { ITree } from 'src/types/responses/tree'
+
+export const API_URL = 'https://test.vmarmysh.com/'
+export const TREE_NAME = 'qwqefdddd'
+
+const instance = axios.create({
+    baseURL: API_URL,
+})
+
+export const rootListAPI = {
+    async getTree() {
+        return await instance.post<ITree>(`api.user.tree.get?treeName=${TREE_NAME}`)
+    },
+    async createTreeNode(parentNodeId: number, nodeName: string) {
+        try {
+            return await instance.post(`api.user.tree.node.create?treeName=${TREE_NAME}&parentNodeId=${parentNodeId}&nodeName=${nodeName}`)
+        } catch (e) {
+            if (axios.isAxiosError(e)) throw new Error(e.response?.data.data.message)
+        }
+    },
+    async deleteTreeNode(nodeId: number) {
+        try {
+            return await instance.post(`api.user.tree.node.delete?treeName=${TREE_NAME}&nodeId=${nodeId}`)
+        } catch (e) {
+            if (axios.isAxiosError(e)) throw new Error(e.response?.data.data.message)
+        }
+    },
+    async renameTreeNode(nodeId: number, newNodeName: string) {
+        try {
+            return await instance.post(`api.user.tree.node.rename?treeName=${TREE_NAME}&nodeId=${nodeId}&newNodeName=${newNodeName}`)
+        } catch (e) {
+            if (axios.isAxiosError(e)) throw new Error(e.response?.data.data.message)
+        }
+    },
+}
